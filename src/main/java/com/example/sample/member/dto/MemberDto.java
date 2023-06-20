5096c2f740e30edbdfff66d1589adc6f7eda8276
hyejik <hyejik@newconnect.co.kr>
2023-06-20 13:54:42 +0900

    검색, 페이징 추가

--- src/main/java/com/example/sample/member/dto/MemberDto.java
+++ src/main/java/com/example/sample/member/dto/MemberDto.java
@@ -5,9 +5,11 @@ import lombok.Data;
 @Data
 public class MemberDto {
 
-    private Long memberId;
+    private Long id;
     private String name;
     private String address;
 
     private String searchOption;
+
+    private String keyword;
 }
--- src/main/java/com/example/sample/member/repository/MemberRepository.java
+++ src/main/java/com/example/sample/member/repository/MemberRepository.java
@@ -1,15 +1,16 @@
 package com.example.sample.member.repository;
 
-import com.example.sample.member.dto.MemberDto;
 import com.example.sample.member.entity.Member;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
-import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Page<Member> findAll(Pageable pageable);
 
 }
