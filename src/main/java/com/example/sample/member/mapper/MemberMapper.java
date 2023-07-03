4c287b826422920ea8b773c0dc0552b16795f520
hyejisc <gpwl0937@gmail.com>
2023-07-03 13:38:51 +0900

    상속 매핑

--- src/main/java/com/example/sample/member/mapper/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.sample.member.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberMapper {
-
-    void updateName();
-}
--- src/main/java/com/example/sample/schedule/UpdateJob.java
+++ src/main/java/com/example/sample/schedule/UpdateJob.java
@@ -1,6 +1,6 @@
 package com.example.sample.schedule;
 
-import com.example.sample.member.mapper.MemberMapper;
+import com.example.sample.mapper.MemberMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
